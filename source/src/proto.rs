c1a265f49513fd802fca0acdc1213d1554a2dccc
Andrew Baxter <>
2025-02-23 04:28:22 +0900

    Don't default to using XDG_RUNTIME_DIR

--- source/src/proto.rs
+++ source/src/proto.rs
@@ -18,8 +18,6 @@ pub const ENV_SOCKET: &str = "PASSWORTH_SOCK";
 pub fn ipc_path() -> PathBuf {
     if let Some(v) = env::var_os(ENV_SOCKET) {
         return PathBuf::from(v);
-    } else if let Some(v) = env::var_os("XDG_RUNTIME_DIR") {
-        return PathBuf::from(v).join("passworth.sock");
     } else {
         return PathBuf::from(DEFAULT_SOCKET);
     }
